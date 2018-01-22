c29f79e7f2093bc00830db428890f53a7298d546
Hưng Nguyễn <1074467+hungnm144@users.noreply.github.com>
2018-01-22 14:43:14 +0700

    Update PaginateRouteServiceProvider.php
    Add new publishes `config/paginateroute.php`

--- src/PaginateRouteServiceProvider.php
+++ src/PaginateRouteServiceProvider.php
@@ -12,7 +12,10 @@ class PaginateRouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../resources/lang' => base_path('resources/lang/vendor/paginateroute'),
+            __DIR__ . '/../resources/lang' => base_path('resources/lang/vendor/paginateroute'),
+        ]);
+        $this->publishes([
+            __DIR__ . '/../config/paginateroute.php' => config_path('paginateroute.php'),
         ]);
     }
 
